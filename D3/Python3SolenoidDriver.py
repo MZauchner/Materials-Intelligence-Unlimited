641be22f43c6c8f87b7b49e45d8ab6c344440b9c
enrico-manfredi <30388890+enrico-manfredi@users.noreply.github.com>
2018-02-02 20:38:27 +0000

    Driver + firmware update
    -Removed strike power modulation feature, as it was not useful
    -Added repeating strike feature in firmware and support over the driver
    -Added a hard-coded ring-down time between strikes
    -Changed pins in script due to hardware changes in control system

--- D3/Python3SolenoidDriver.py
+++ D3/Python3SolenoidDriver.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-def hammer_time(com, baud, impact_time, head_pos, delay, pwr):
+def hammer_time(com, baud, impact_time, head_pos, delay, reps):
     ser = serial.Serial(com_port, baud_rate)
     ##wait for serial port to initialise
     time.sleep(3)
@@ -9,7 +9,7 @@ def hammer_time(com, baud, impact_time, head_pos, delay, pwr):
     ser.write((str(impact_time) + " " +  
                str(head_pos) + " " +
                str(delay) + " " +
-               str(pwr)).encode("utf-8"))
+               str(reps)).encode("utf-8"))
     ##waits for the arduino to finish recieving information
     time.sleep(3)
     ##debug statement for checking what command was sent
@@ -18,16 +18,19 @@ def hammer_time(com, baud, impact_time, head_pos, delay, pwr):
 
 ##Serial port  - currently set for windows
 ##These need to match the values in the arduino script!
-com_port = "COM4"
+com_port = "COM5"
 baud_rate = 115200
 
 ##Pause between servo movement and testing
 ##use 500 for testing and 2000 for actual operation
-delay = 500
+delay = 2000
+repeats = 5
 
-##intercalated power and impact time test - will be used for force calibration
-for power in range(800, 1030, 10):
-    for impact_time in range(1,21):
-        hammer_time(com_port, baud_rate, impact_time, 120, delay, power)
-        print("Power level:" + str(power))
-        print("Switch-on Time:" + str(impact_time))
+hammer_time(com_port, baud_rate, 7, 180, delay, repeats)
+
+###intercalated power and impact time test - will be used for force calibration
+#for power in range(800, 1030, 10):
+#    for impact_time in range(1,21):
+#        hammer_time(com_port, baud_rate, impact_time, 120, delay, power)
+#        print("Power level:" + str(power))
+#        print("Switch-on Time:" + str(impact_time))
--- D3/push_mode_tapping.ino
+++ D3/push_mode_tapping.ino
@@ -3,16 +3,15 @@
 Servo head_rotate;
 
 //Pin assignments
-int solenoid_switch = 3; //solenoid control signal pin
-int servo_pin = 9; //servo control signal pin
+int solenoid_switch = 2; //solenoid control signal pin
+int servo_pin = 6; //servo control signal pin
 
 //operating variables
 int impact_time;
 int head_pos;
 int delay_time;
-int strike_repeats;
-int strike_power;
-int centre_val = 82; //this is the central position - has to be calibrated on each assembly
+int repeats;
+int centre_val = 90; //this is the central position - has to be calibrated on each assembly
 
 void setup() {
 pinMode(solenoid_switch, OUTPUT);
@@ -25,14 +24,16 @@ void loop() {
 if (Serial.available()){
   String input = Serial.readString();
   Serial.println(input);
-  sscanf(input.c_str(), "%d %d %d %d", &impact_time, &head_pos, &delay_time, &strike_power);
+  sscanf(input.c_str(), "%d %d %d %d", &impact_time, &head_pos, &delay_time, &repeats);
   head_rotate.write(head_pos);
   delay(delay_time);
-  analogWrite(solenoid_switch, strike_power);
-  delay(impact_time);
-  digitalWrite(solenoid_switch, LOW);
+  for (int i = 0; i < repeats; i++){
+    digitalWrite(solenoid_switch, HIGH);
+    delay(impact_time);
+    digitalWrite(solenoid_switch, LOW); 
+    delay(50); //hard-coded value to allow for sample ring-down between strikes
+  }
   delay(delay_time);
   head_rotate.write(centre_val);
 }
 }
-
