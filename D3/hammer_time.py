78aff776076f6c0280eb4c96377e43097da1e5a1
MZauchner <mario.zauchner39@gmx.net>
2018-02-07 11:40:34 +0000

    all

---
+++ D3/hammer_time.py
@@ -0,0 +1,37 @@
+import serial
+import time
+
+def hammer_time(com, baud, impact_time, head_pos, delay, reps):
+    ser = serial.Serial(com_port, baud_rate)
+    ##wait for serial port to initialise
+    time.sleep(3)
+    ##Composes the command string
+    ser.write((str(impact_time) + " " +  
+               str(head_pos) + " " +
+               str(delay) + " " +
+               str(reps)).encode("utf-8"))
+    ##waits for the arduino to finish recieving information
+    time.sleep(3)
+    ##debug statement for checking what command was sent
+    print(ser.readline())
+    ser.close()
+
+##Serial port  - currently set for windows
+##These need to match the values in the arduino script!
+com_port = "/dev/ttyUSB0"
+baud_rate = 115200
+
+##Pause between servo movement and testing
+##use 500 for testing and 2000 for actual operation
+delay = 2000
+repeats = 5
+rodpos = 180
+slidepos = 20
+hammer_time(com_port, baud_rate, 7, rodpos, delay, repeats)
+
+###intercalated power and impact time test - will be used for force calibration
+#for power in range(800, 1030, 10):
+#    for impact_time in range(1,21):
+#        hammer_time(com_port, baud_rate, impact_time, 120, delay, power)
+#        print("Power level:" + str(power))
+#        print("Switch-on Time:" + str(impact_time))
