ce3930f53dc86790b69fa3660c5d1f3fe3d47910
MZauchner <mario.zauchner39@gmx.net>
2018-03-07 12:25:01 +0000

    update

--- D2/slide_holder.py
+++ D2/slide_holder.py
@@ -14,12 +14,12 @@ def slideholder(motion):
 
     # Assign numerals to send to Arduino for a given string
     if motion == "cw":
-        arduinoData.write("1".encode("utf-8")) # Send over data as bytes to Arduino
+        arduinoData.write('1'.encode("utf-8")) # Send over data as bytes to Arduino
 
     elif motion == "ccw":
-        arduinoData.write("2".encode("utf-8"))
+        arduinoData.write('2'.encode("utf-8"))
 
     elif motion == "both":
-        arduinoData.write("3".encode("utf-8"))
+        arduinoData.write('3'.encode("utf-8"))
 
     arduinoData.close()
---
+++ D4/Conveyer_Arduino/Conveyer_Arduino.ino
@@ -0,0 +1,45 @@
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *myMotor = AFMS.getMotor(4);
+ 
+void setup() {
+  AFMS.begin();
+  Serial.begin(9600);           
+  Serial.println("Motor test!");
+  
+  myMotor->setSpeed(80);     
+}
+ 
+void loop() 
+{
+  if(Serial.available()){
+  int input=Serial.parseInt();
+  Serial.println(input) ;
+  if (input == 1 ){      //accept
+    myMotor->run(RELEASE);      
+  delay(1000);
+    myMotor->run(BACKWARD);      
+  delay(3000);
+    myMotor->run(RELEASE);    
+  delay(1000);
+  }
+  
+  if (input == 2 ){      //reject
+    myMotor->run(RELEASE);      
+  delay(1000);
+    myMotor->run(BACKWARD);      
+  delay(500);
+   myMotor->run(RELEASE);      
+  delay(1000);
+    myMotor->run(FORWARD);   
+  delay(2000);
+    myMotor->run(RELEASE);    
+  delay(1000);
+  }
+}
+}
