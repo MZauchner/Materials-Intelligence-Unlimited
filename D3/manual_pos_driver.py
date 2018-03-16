f5383791369d47996f0ea4f1896078160bfa0bdb
enrico-manfredi <30388890+enrico-manfredi@users.noreply.github.com>
2018-03-16 10:39:35 +0000

    tweaked values
    Changed the sample position values based on calibration to new machine

--- D3/manual_pos_driver.py
+++ D3/manual_pos_driver.py
@@ -9,9 +9,9 @@ time.sleep(3)
 
 ##Sample specific parameters
 rod_strike = 7
-rod_pos = 30
+rod_pos = 137
 slide_strike = 10
-slide_pos = 160
+slide_pos = 0
 
 ##General parameters
 ##Serial port  - currently set for windows
